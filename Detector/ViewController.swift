d667d3a86e19d74c2f1401822a30a08202cc217b
kangoo <kangoo@unused-136-140.via.ecp.fr>
2017-02-09 16:37:22 +0100

    feat: Squares around face and eyes

--- Detector/ViewController.swift
+++ Detector/ViewController.swift
@@ -15,10 +15,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        personPic.image = UIImage(named: "face-1")
+        personPic.image = UIImage(named: "face-8")
 
         detect()
     }
+    
     
     func detect() {
         
@@ -54,19 +55,44 @@ class ViewController: UIViewController {
             faceViewBounds.origin.y += offsetY
             
             let faceBox = UIView(frame: faceViewBounds)
-            
             faceBox.layer.borderWidth = 3
             faceBox.layer.borderColor = UIColor.red.cgColor
             faceBox.backgroundColor = UIColor.clear
             personPic.addSubview(faceBox)
             
+            let eyeSize = CGSize(width: 5, height: 5)
+            
             if face.hasLeftEyePosition {
                 print("Left eye bounds are \(face.leftEyePosition)")
             }
             
+            var leftEyeBounds = CGRect(x: face.leftEyePosition.x, y: face.leftEyePosition.y, width: eyeSize.width, height: eyeSize.height).applying(transform)
+            
+            leftEyeBounds = leftEyeBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+            leftEyeBounds.origin.x += offsetX
+            leftEyeBounds.origin.y += offsetY
+            
+            let leftEyeBox = UIView(frame: leftEyeBounds)
+            leftEyeBox.layer.borderWidth = 3
+            leftEyeBox.layer.borderColor = UIColor.red.cgColor
+            leftEyeBox.backgroundColor = UIColor.clear
+            personPic.addSubview(leftEyeBox)
+            
             if face.hasRightEyePosition {
                 print("Right eye bounds are \(face.rightEyePosition)")
             }
+            
+            var rightEyeBounds = CGRect(x: face.rightEyePosition.x, y: face.rightEyePosition.y, width: eyeSize.width, height: eyeSize.height).applying(transform)
+            
+            rightEyeBounds = rightEyeBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+            rightEyeBounds.origin.x += offsetX
+            rightEyeBounds.origin.y += offsetY
+            
+            let rightEyeBox = UIView(frame: rightEyeBounds)
+            rightEyeBox.layer.borderWidth = 3
+            rightEyeBox.layer.borderColor = UIColor.red.cgColor
+            rightEyeBox.backgroundColor = UIColor.clear
+            personPic.addSubview(rightEyeBox)
         }
     }
     
